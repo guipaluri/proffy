94a1d9ec265306e618cc09de3fe44d76fdc64778
guipaluri <guilhermepaluri@hotmail.com>
2022-12-29 11:58:42 -0300

    Finished server :tada:

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json())
+app.use(routes);
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' })
-});
-
-app.listen(3333, () => {
-    console.log('Server running on port: 3333')
-});
+app.listen(3333);
