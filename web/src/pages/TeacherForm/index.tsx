094a3663e031738b32a3ff58fae2bdb1865c477a
guipaluri <guilhermepaluri@hotmail.com>
2022-12-30 16:13:01 -0300

    Finished front-end

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css';
+import Textarea from '../../components/Textarea';
 
 function TeacherForm() {
     return (
@@ -16,21 +20,31 @@ function TeacherForm() {
                 <fieldset>
                     <legend>Seus dados</legend>
 
-                    <div className="input-block">
-                        <label htmlFor="name">Nome completo</label>
-                        <input type="text" id='name' />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="avatar">Avatar</label>
-                        <input type="text" id='avatar' />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="whatsapp">WhatsApp</label>
-                        <input type="text" id='whatsapp' />
-                    </div>
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="WhatsApp" />
+                    <Textarea name="bio" label="Biografia" />
+                    
                 </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Input name="subject" label="Matéria" />
+                    <Input name="cost" label="Custo da sua hora por aula" />
+                    
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={ warningIcon } alt="Aviso importante" />
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
             </main>
         </div>
     );
